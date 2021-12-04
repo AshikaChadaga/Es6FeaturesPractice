c8f7e5e5217774b6f9a0da6a56ff2fe8c3b713aa
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-04 18:02:01 +0530

    [Ashika]ADDED: Demonstrating multiline strings

---
+++ mulitine_strings.js
@@ -0,0 +1,12 @@
+const quote = `MARVEL QUOTES    
+That’s my secret, Captain. I’m always angry.
+I love you 3000.
+Cause if we can’t protect the Earth, you can be damn sure we’ll avenge it.
+If we’re going to win this fight, some of us might have to lose it.
+I can do this all day.
+You get hurt, hurt ’em back. You get killed… walk it off.
+Whatever it takes.
+The city is flying and we’re fighting an army of robots, and I have a bow and arrow. None of this makes sense.
+I still believe in heroes.
+Avengers, assemble!`;
+console.log(quote);
